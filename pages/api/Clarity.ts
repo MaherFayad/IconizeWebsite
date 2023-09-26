73e9c0dbf20b244d1c48d566bbfddfb049802d12
Maher Fayad <MaherFayad55@Gmail.com>
2023-09-26 21:57:34 +0300

    123

--- pages/api/Clarity.ts
+++ pages/api/Clarity.ts
@@ -3,17 +3,18 @@ import React, { useEffect } from 'react';
 function ClarityScript() {
   useEffect(() => {
     // Define the Clarity script as a function
-    (function(c: any, l: any, a: string, r: string, i: string, t: any, y: any) {
+    (function(c: any, l: any, a: string, r: string, i: string) {
       c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-      t = l.createElement(r);
+      const t = l.createElement(r);
       t.async = true;
       t.src = `https://www.clarity.ms/tag/${i}`;
-      y = l.getElementsByTagName(r)[0];
+      const y = l.getElementsByTagName(r)[0];
       y.parentNode.insertBefore(t, y);
-    })(window, document, "clarity", "script", "j1aw65gcrm", undefined, undefined);
+    })(window, document, "clarity", "script", "j1aw65gcrm");
   }, []);
 
-  return null; // or you can return a React fragment if needed
+  // You can return a React element here if needed
+  return null;
 }
 
 export default ClarityScript;
