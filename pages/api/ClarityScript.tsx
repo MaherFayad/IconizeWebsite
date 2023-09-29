fae362057f60b096d3b942a684656e4d9511a5f9
Maher Fayad <MaherFayad55@Gmail.com>
2023-09-29 17:44:44 +0300

    123

--- pages/api/ClarityScript.tsx
+++ pages/api/ClarityScript.tsx
@@ -1,18 +1,10 @@
 import { useEffect } from 'react';
+import { clarity } from 'react-microsoft-clarity';
+
 
 const ClarityScript = () => {
   useEffect(() => {
-    // Clarity script code
-    (function (c: any, l: any, a: string, r: string, i: string, t: any, y: HTMLElement | undefined) {
-      c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments); };
-      t = l.createElement(r);
-      t.async = 1;
-      t.src = "https://www.clarity.ms/tag/" + i;
-      y = l.getElementsByTagName(r)[0] as HTMLElement | undefined; // Explicitly type y
-      if (y) {
-        y.parentNode?.insertBefore(t, y);
-      }
-    })(window, document, "clarity", "script", "j1aw65gcrm", undefined, undefined);
+    clarity.init("j1aw65gcrm");
   }, []); // Empty dependency array to run the effect once
 
   return null; // You can return null since this component doesn't render anything
