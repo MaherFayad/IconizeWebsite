2b26d7593e52e0993374f373a970770d94b6f118
Maher Fayad <63962002+MaherFayad@users.noreply.github.com>
2023-10-15 15:36:26 +0200

    123

--- templates/AboutPage/index.tsx
+++ templates/AboutPage/index.tsx
@@ -15,7 +15,7 @@ const AboutPage = () => {
             <Story />
             <Preview
                 className="section-mb160"
-                image="https://firebasestorage.googleapis.com/v0/b/iconize-website.appspot.com/o/images%2Fimage-1.jpg?alt=media&token=f4b28803-a922-497e-95fd-d71818d61a24&_gl=1*1sm34tr*_ga*MjAyNTM5Nzk0Mi4xNjk3MzU2ODg2*_ga_CW55HF8NVT*MTY5NzM1Njg4Ni4xLjEuMTY5NzM2MzU4Mi44LjAuMA.."
+                image="https://firebasestorage.googleapis.com/v0/b/iconize-website.appspot.com/o/images%2Fimage-1.jpg?alt=media&token=ba6486c3-7e0b-4771-8746-f2663de817c3&_gl=1*1fejaaj*_ga*MjAyNTM5Nzk0Mi4xNjk3MzU2ODg2*_ga_CW55HF8NVT*MTY5NzM3MzY1Ny4yLjEuMTY5NzM3Njk3NC41OC4wLjA."
                 wide
             />
             <Values />
