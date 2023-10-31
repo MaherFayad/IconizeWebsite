679fb0eb83c7c54e1b226d2074c6f44448c26fca
Maher Fayad <63962002+MaherFayad@users.noreply.github.com>
2023-10-31 10:09:58 +0200

    12

--- mocks/team.tsx
+++ mocks/team.tsx
@@ -1,163 +1,19 @@
 export const team = [
     {
         id: "0",
-        title: "Jensen Mustafa",
-        position: "CEO AND FOUNDER",
+        title: "loerum ipslum",
+        position: "loerum ipslum",
         photo: "/images/team/photo-1.jpg",
         description:
-            "This strategist quickly became interested in new technologies that are revolutionizing communications. He is a key player in the implementation of Facebook and Twitter campaigns in Indonesia with Keeper, particularly in the B2B, luxury, and politics sectors. His true strength, however, is his ability to change beliefs.",
+            "loerum ipslum",
         socials: [
             {
-                icon: "instagram",
-                url: "https://www.instagram.com/ui8net/",
+                icon: "loerum ipslum",
+                url: "https://www.instagram.com/loerum ipslum/",
             },
             {
-                icon: "linkedin",
-                url: "https://www.linkedin.com/company/ui8",
-            },
-        ],
-    },
-    {
-        id: "1",
-        title: "Louisa Carson",
-        position: "STRATEGIST & PROJECT MANAGEMENT",
-        photo: "/images/team/photo-2.jpg",
-        description:
-            "This strategist quickly became interested in new technologies that are revolutionizing communications. He is a key player in the implementation of Facebook and Twitter campaigns in Indonesia with Keeper, particularly in the B2B, luxury, and politics sectors. His true strength, however, is his ability to change beliefs.",
-        socials: [
-            {
-                icon: "instagram",
-                url: "https://www.instagram.com/ui8net/",
-            },
-            {
-                icon: "linkedin",
-                url: "https://www.linkedin.com/company/ui8",
-            },
-        ],
-    },
-    {
-        id: "2",
-        title: "Evan Cote",
-        position: "CREATIVE DIRECTOR",
-        photo: "/images/team/photo-3.jpg",
-        description:
-            "This strategist quickly became interested in new technologies that are revolutionizing communications. He is a key player in the implementation of Facebook and Twitter campaigns in Indonesia with Keeper, particularly in the B2B, luxury, and politics sectors. His true strength, however, is his ability to change beliefs.",
-        socials: [
-            {
-                icon: "instagram",
-                url: "https://www.instagram.com/ui8net/",
-            },
-            {
-                icon: "linkedin",
-                url: "https://www.linkedin.com/company/ui8",
-            },
-        ],
-    },
-    {
-        id: "3",
-        title: "Nelly Bowes",
-        position: "PROJECT MANAGER",
-        photo: "/images/team/photo-4.jpg",
-        description:
-            "This strategist quickly became interested in new technologies that are revolutionizing communications. He is a key player in the implementation of Facebook and Twitter campaigns in Indonesia with Keeper, particularly in the B2B, luxury, and politics sectors. His true strength, however, is his ability to change beliefs.",
-        socials: [
-            {
-                icon: "instagram",
-                url: "https://www.instagram.com/ui8net/",
-            },
-            {
-                icon: "linkedin",
-                url: "https://www.linkedin.com/company/ui8",
-            },
-        ],
-    },
-    {
-        id: "4",
-        title: "Ezekiel Whitmore",
-        position: "DIGITAL MEDIA SPECIALIST",
-        photo: "/images/team/photo-5.jpg",
-        description:
-            "This strategist quickly became interested in new technologies that are revolutionizing communications. He is a key player in the implementation of Facebook and Twitter campaigns in Indonesia with Keeper, particularly in the B2B, luxury, and politics sectors. His true strength, however, is his ability to change beliefs.",
-        socials: [
-            {
-                icon: "instagram",
-                url: "https://www.instagram.com/ui8net/",
-            },
-            {
-                icon: "linkedin",
-                url: "https://www.linkedin.com/company/ui8",
-            },
-        ],
-    },
-    {
-        id: "5",
-        title: "Essa Butler",
-        position: "DIGITAL MARKETING SPECIALIST",
-        photo: "/images/team/photo-6.jpg",
-        description:
-            "This strategist quickly became interested in new technologies that are revolutionizing communications. He is a key player in the implementation of Facebook and Twitter campaigns in Indonesia with Keeper, particularly in the B2B, luxury, and politics sectors. His true strength, however, is his ability to change beliefs.",
-        socials: [
-            {
-                icon: "instagram",
-                url: "https://www.instagram.com/ui8net/",
-            },
-            {
-                icon: "linkedin",
-                url: "https://www.linkedin.com/company/ui8",
-            },
-        ],
-    },
-    {
-        id: "6",
-        title: "Zakaria Sparrow",
-        position: "UX/UI DESIGN SPECIALIST",
-        photo: "/images/team/photo-7.jpg",
-        description:
-            "This strategist quickly became interested in new technologies that are revolutionizing communications. He is a key player in the implementation of Facebook and Twitter campaigns in Indonesia with Keeper, particularly in the B2B, luxury, and politics sectors. His true strength, however, is his ability to change beliefs.",
-        socials: [
-            {
-                icon: "instagram",
-                url: "https://www.instagram.com/ui8net/",
-            },
-            {
-                icon: "linkedin",
-                url: "https://www.linkedin.com/company/ui8",
-            },
-        ],
-    },
-    {
-        id: "7",
-        title: "Arfa Estes",
-        position: "SENIOR GRAPHIC DESIGNER",
-        photo: "/images/team/photo-8.jpg",
-        description:
-            "This strategist quickly became interested in new technologies that are revolutionizing communications. He is a key player in the implementation of Facebook and Twitter campaigns in Indonesia with Keeper, particularly in the B2B, luxury, and politics sectors. His true strength, however, is his ability to change beliefs.",
-        socials: [
-            {
-                icon: "instagram",
-                url: "https://www.instagram.com/ui8net/",
-            },
-            {
-                icon: "linkedin",
-                url: "https://www.linkedin.com/company/ui8",
-            },
-        ],
-    },
-    {
-        id: "8",
-        title: "Dayna Humphreys",
-        position: "DIGITAL MARKETING SPECIALIST",
-        photo: "/images/team/photo-9.jpg",
-        description:
-            "This strategist quickly became interested in new technologies that are revolutionizing communications. He is a key player in the implementation of Facebook and Twitter campaigns in Indonesia with Keeper, particularly in the B2B, luxury, and politics sectors. His true strength, however, is his ability to change beliefs.",
-        socials: [
-            {
-                icon: "instagram",
-                url: "https://www.instagram.com/ui8net/",
-            },
-            {
-                icon: "linkedin",
-                url: "https://www.linkedin.com/company/ui8",
+                icon: "loerum ipslum",
+                url: "https://www.linkedin.com/company/loerum ipslum",
             },
         ],
     },
