4c2bc4e3697a253cf227dd860d1f01c06aaa9a1a
Maher Fayad <MaherFayad55@Gmail.com>
2023-11-07 14:29:00 +0200

    12

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true},
+    unoptimized: true,
+    },
+  swcMinify: true,
+  reactStrictMode: true,
   output: 'standalone',
   trailingSlash: true,
 }
